47c189c8089c1076b570c118deb6e72b12d18ac0
Andreas Larsen <andreas@northcode.no>
2014-06-04 09:57:17 +0200

    starting to recode screens to use sfml

--- virtual-machine/screen.hpp
+++ virtual-machine/screen.hpp
@@ -4,24 +4,46 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+struct screen_color
+{
+  byte R,G,B;
+};
+
 class screen : public device
 {
 public:
-  int height{600},width{800};
+  maddr height{600},width{800};
   sf::RenderWindow window{{width,height},"NVM3"};
+  sf::Image vram;
+  sf::Texture buffer;
+  sf::Sprite sprite;
 
   screen();
   ~screen();
 
   void update();
 
-  void set_pixel(int x, int y, )
+  void set_pixel(maddr, maddr, screen_color);
 };
 
 screen::screen() {
-
+  vram.create(height,width,sf::Color::Black);
+  buffer = sf::Texture();
+  buffer.loadFromImage(vram);
+  sprite = sf::Sprite(buffer);
 }
 
 screen::~screen() {
 
+}
+
+void screen::update() {
+  window.clear(sf::Color::Black);
+  window.draw(sprite);
+  window.display();
+}
+
+void screen::set_pixel(maddr x, maddr y, screen_color color) {
+  vram.setPixel(x,y,sf::Color(color.R,color.G,color.B));
+  buffer.loadFromImage(vram);
 }
